e38b71a2c364f58a7b378171f4c44efcd8a9cd0b
lautaro <lniccolini600@alumnos.iua.edu.ar>
2024-08-05 10:17:21 -0300

    Ejemplo1

---
+++ Ejemplo1.py
@@ -0,0 +1,11 @@
+from math import pi
+#CALCULAR EL VOLUMEN DE UNA ESFERA#
+
+cadena_leida=input("Dame el radio \n")
+
+radio= float(cadena_leida)
+
+volumen= 4/3 * pi * radio **3
+print("El volumen es:" , volumen , "metros cubicos") #la , va a permitir separar la cadena
+print("Volumen " + str(volumen) + "metros cubicos") #Va a concatenar
+print("Volumen  {0:.4f} metros cubicos".format(volumen)) #Uso formato
